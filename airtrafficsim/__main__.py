fed8e74bf6d7ce26384bf1dc2371627f0a5688b6
Frankie Hui <kyfrankie@gmail.com>
2022-10-13 01:27:18 +0800

    Update __main__.py

--- airtrafficsim/__main__.py
+++ airtrafficsim/__main__.py
@@ -7,7 +7,7 @@ import airtrafficsim.server.server as server
 def main():
     # Give error if BADA data is missing TODO: To be removed when OpenAP is implemented
     if len(os.listdir('data/performance/BADA/')) <= 1:
-        raise IOError("BADA folder is empty. Remember to put the BADA performance data into /data/BADA/.")
+        raise IOError("BADA folder is empty. Remember to put the BADA performance data into /data/performance/BADA/.")
 
     if len(sys.argv) > 1 and sys.argv[1] == '--headless':
         # Run user defined environment without UI: python -m airtrafficsim --headless <env name>
